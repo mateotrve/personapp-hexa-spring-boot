9f0e2d835eeabacf2997708cebb77e7fb57be1a4
mateotrve <mathe.andres99@gmail.com>
2023-05-25 21:32:57 -0500

    Adaptador Mongo DB terminado

---
+++ mongo-output-adapter/src/main/java/co/edu/javeriana/as/personapp/mongo/repository/TelefonoRepositoryMongo.java
@@ -0,0 +1,11 @@
+package co.edu.javeriana.as.personapp.mongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import co.edu.javeriana.as.personapp.mongo.document.TelefonoDocument;
+
+@Repository
+public interface TelefonoRepositoryMongo extends MongoRepository<TelefonoDocument, String>{
+    
+}
